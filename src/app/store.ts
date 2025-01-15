494fe46978584d791ed9608d6e0e47afc85366cf
farabek <farhodmuhamadiev4@gmail.com>
2025-01-15 10:31:33 +0500

    made lesson 18-5

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,6 @@
 // import { configureStore } from "@reduxjs/toolkit"
+// import { setupListeners } from "@reduxjs/toolkit/query"
+// import { todolistsApi } from "features/todolists/api/todolistsApi"
 // import { authReducer, authSlice } from "../features/auth/model/authSlice"
 // import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
 // import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
@@ -10,22 +12,29 @@
 //     [todolistsSlice.name]: todolistsReducer,
 //     [appSlice.name]: appReducer,
 //     [authSlice.name]: authReducer,
+//     [todolistsApi.reducerPath]: todolistsApi.reducer,
 //   },
+//   // 1
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
 // })
+
+// // 2
+// setupListeners(store.dispatch)
 
 // export type RootState = ReturnType<typeof store.getState>
 
 // export type AppDispatch = typeof store.dispatch
 
-//////////////////////////////////////////
+//////////////////////////////////////////////////////
 
 import { configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { todolistsApi } from "features/todolists/api/todolistsApi"
+// import { todolistsApi } from "features/todolists/api/todolistsApi"
 import { authReducer, authSlice } from "../features/auth/model/authSlice"
 import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
 import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
 import { appReducer, appSlice } from "./appSlice"
+import { baseApi } from "./baseApi"
 
 export const store = configureStore({
   reducer: {
@@ -33,10 +42,12 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
-    [todolistsApi.reducerPath]: todolistsApi.reducer,
+    // [todolistsApi.reducerPath]: todolistsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
   // 1
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 // 2
--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -9,6 +9,7 @@
 // export const todolistsApi = createApi({
 //   // 3
 //   reducerPath: "todolistsApi",
+//   tagTypes: ["Todolist"],
 //   // 4
 //   baseQuery: fetchBaseQuery({
 //     baseUrl: process.env.REACT_APP_BASE_URL,
@@ -18,41 +19,59 @@
 //     },
 //   }),
 //   // 5
-//   //1 вариант с return
-//   // endpoints: (build) => {
-//   //   return {
-//   //     // 6
-//   //     // getTodolists: build.query<any[], void>({
-//   //     getTodolists: build.query<DomainTodolist[], void>({
-//   //       // //1 вариант
-//   //       // query: () => {
-//   //       //   return {
-//   //       //     url: "todo-lists",
-//   //       //     method: "GET",
-//   //       //   }
-//   //       // },
-//   //       //2 вариавнт-сокращенный
-//   //       query: () => "todo-lists",
-
-//   //       transformResponse(todolists: Todolist[]): DomainTodolist[] {
-//   //         return todolists.map((tl) => ({ ...tl, filter: "all", entityStatus: "idle" }))
-//   //       },
-//   //     }),
-//   //   }
-//   // },
-//   //  2 вариант без return
 //   endpoints: (build) => ({
 //     getTodolists: build.query<DomainTodolist[], void>({
 //       query: () => "todo-lists",
 //       transformResponse(todolists: Todolist[]): DomainTodolist[] {
 //         return todolists.map((tl) => ({ ...tl, filter: "all", entityStatus: "idle" }))
 //       },
+//       providesTags: ["Todolist"],
+//     }),
+
+//     addTodolist: build.mutation<BaseResponse<{ item: Todolist }>, string>({
+//       query: (title) => {
+//         return {
+//           url: "todo-lists",
+//           method: "POST",
+//           body: { title },
+//         }
+//       },
+//       invalidatesTags: ["Todolist"],
+//     }),
+
+//     removeTodolist: build.mutation<BaseResponse, string>({
+//       query: (id) => {
+//         return {
+//           method: "DELETE",
+//           url: `todo-lists/${id}`,
+//         }
+//       },
+//       invalidatesTags: ["Todolist"],
+//     }),
+
+//     updateTodolistTitle: build.mutation<BaseResponse, { id: string; title: string }>({
+//       query: ({ id, title }) => {
+//         return {
+//           method: "PUT",
+//           url: `todo-lists/${id}`,
+//           body: {
+//             title,
+//           },
+//         }
+//       },
+//       invalidatesTags: ["Todolist"],
 //     }),
 //   }),
 // })
 
 // // 7
-// export const { useGetTodolistsQuery } = todolistsApi
+// // export const { useGetTodolistsQuery } = todolistsApi
+// export const {
+//   useGetTodolistsQuery,
+//   useAddTodolistMutation,
+//   useRemoveTodolistMutation,
+//   useUpdateTodolistTitleMutation,
+// } = todolistsApi
 
 // ///////////////////////////////////////////
 
@@ -75,27 +94,30 @@
 /////////////////////////////////////////////
 
 // 1
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { instance } from "common/instance"
 import { BaseResponse } from "common/types"
 import { Todolist } from "./todolistsApi.types"
 import { DomainTodolist } from "../model/todolistsSlice"
+import { baseApi } from "app/baseApi"
 
 // 2
-export const todolistsApi = createApi({
-  // 3
-  reducerPath: "todolistsApi",
-  tagTypes: ["Todolist"],
-  // 4
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
-      headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
-    },
-  }),
-  // 5
+// export const todolistsApi = createApi({
+export const todolistsApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
+    // // 3
+    // reducerPath: "todolistsApi",
+    // tagTypes: ["Todolist"],
+    // // 4
+    // baseQuery: fetchBaseQuery({
+    //   baseUrl: process.env.REACT_APP_BASE_URL,
+    //   prepareHeaders: (headers) => {
+    //     headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
+    //     headers.set("Authorization", `Bearer ${localStorage.getItem("sn-token")}`)
+    //   },
+    // }),
+    // // 5
+    // endpoints: (build) => ({
     getTodolists: build.query<DomainTodolist[], void>({
       query: () => "todo-lists",
       transformResponse(todolists: Todolist[]): DomainTodolist[] {
