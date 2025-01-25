542a6c4b90dd6922c6554403e1e019692985d918
farabek <farhodmuhamadiev4@gmail.com>
2025-01-25 11:27:29 +0500

    lesson 19-3-2

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,4 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit"
+// import { createSlice } from "@reduxjs/toolkit"
+
+// export type ThemeMode = "dark" | "light"
+// export type RequestStatus = "idle" | "loading" | "succeeded" | "failed"
+
+// export const appSlice = createSlice({
+//   name: "app",
+//   initialState: {
+//     themeMode: "light" as ThemeMode,
+//     status: "idle" as RequestStatus,
+//     error: null as string | null,
+//     isLoggedIn: false,
+//   },
+//   reducers: (create) => ({
+//     changeTheme: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+//       state.themeMode = action.payload.themeMode
+//     }),
+//     setAppStatus: create.reducer<{ status: RequestStatus }>((state, action) => {
+//       state.status = action.payload.status
+//     }),
+//     setAppError: create.reducer<{ error: string | null }>((state, action) => {
+//       state.error = action.payload.error
+//     }),
+//     setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+//       state.isLoggedIn = action.payload.isLoggedIn
+//     }),
+//   }),
+//   selectors: {
+//     selectThemeMode: (state) => state.themeMode,
+//     selectAppStatus: (state) => state.status,
+//     selectAppError: (state) => state.error,
+//     selectIsLoggedIn: (state) => state.isLoggedIn,
+//   },
+// })
+
+// export const { changeTheme, setAppError, setAppStatus, setIsLoggedIn } = appSlice.actions
+// export const { selectAppStatus, selectAppError, selectThemeMode, selectIsLoggedIn } = appSlice.selectors
+// export const appReducer = appSlice.reducer
+
+///////////////////////////////////////////////////
+
+// import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, isPending, isFulfilled, isRejected } from "@reduxjs/toolkit"
+import { tasksApi } from "features/todolists/api/tasksApi"
+import { todolistsApi } from "features/todolists/api/todolistsApi"
 
 export type ThemeMode = "dark" | "light"
 export type RequestStatus = "idle" | "loading" | "succeeded" | "failed"
@@ -25,6 +69,46 @@ export const appSlice = createSlice({
       state.isLoggedIn = action.payload.isLoggedIn
     }),
   }),
+
+  // extraReducers: (builder) => {
+  //   builder
+  //     .addMatcher(
+  //       (action) => {
+  //         return action.type.endsWith("/fulfilled")
+  //       },
+  //       (state) => {
+  //         state.status = "succeeded"
+  //       },
+  //     )
+  //     .addMatcher(
+  //       (action) => {
+  //         return action.type.endsWith("/rejected")
+  //       },
+  //       (state) => {
+  //         state.status = "failed"
+  //       },
+  //     )
+  // },
+
+  extraReducers: (builder) => {
+    builder
+      .addMatcher(isPending, (state, action) => {
+        if (
+          todolistsApi.endpoints.getTodolists.matchPending(action) ||
+          tasksApi.endpoints.getTasks.matchPending(action)
+        ) {
+          return
+        }
+        state.status = "loading"
+      })
+      .addMatcher(isFulfilled, (state) => {
+        state.status = "succeeded"
+      })
+      .addMatcher(isRejected, (state) => {
+        state.status = "failed"
+      })
+  },
+
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectAppStatus: (state) => state.status,
