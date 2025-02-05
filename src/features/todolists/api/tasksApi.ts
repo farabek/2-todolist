0ce29811592261e658ae6d20819f2cf4395b3bc6
farabek <farhodmuhamadiev4@gmail.com>
2025-02-05 11:35:36 +0500

    lesson 20-4-3

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -140,36 +140,110 @@
 
 ////////////////////////////////////////////
 
+// import { BaseResponse } from "common/types"
+// import { baseApi } from "../../../app/baseApi"
+// import { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
+
+// export const PAGE_SIZE = 4
+
+// export const tasksApi = baseApi.injectEndpoints({
+//   endpoints: (build) => ({
+//     // getTasks: build.query<GetTasksResponse, string>({
+//     getTasks: build.query<GetTasksResponse, { todolistId: string; args: { count: number; page: number } }>({
+//       // query: (todolistId) => `todo-lists/${todolistId}/tasks`,
+//       query: ({ todolistId, args }) => {
+//         const params = { ...args, count: PAGE_SIZE }
+
+//         // return {
+//         //   url: `todo-lists/${todolistId}/tasks`,
+//         //   params: args,
+//         // }
+//         return {
+//           method: "GET",
+//           url: `todo-lists/${todolistId}/tasks`,
+//           params,
+//         }
+//       },
+//       providesTags: (res, err, { todolistId }) =>
+//         res
+//           ? [...res.items.map(({ id }) => ({ type: "Task", id }) as const), { type: "Task", id: todolistId }]
+//           : ["Task"],
+//     }),
+
+//     addTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
+//       query: ({ todolistId, title }) => {
+//         return {
+//           method: "POST",
+//           url: `todo-lists/${todolistId}/tasks`,
+//           body: {
+//             title,
+//           },
+//         }
+//       },
+//       invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
+//     }),
+//     removeTask: build.mutation<BaseResponse, { todolistId: string; taskId: string }>({
+//       query: ({ todolistId, taskId }) => {
+//         return {
+//           method: "DELETE",
+//           url: `todo-lists/${todolistId}/tasks/${taskId}`,
+//         }
+//       },
+//       invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
+//     }),
+//     updateTask: build.mutation<
+//       BaseResponse<{ item: DomainTask }>,
+//       { todolistId: string; taskId: string; model: UpdateTaskModel }
+//     >({
+//       query: ({ todolistId, taskId, model }) => {
+//         return {
+//           method: "PUT",
+//           url: `todo-lists/${todolistId}/tasks/${taskId}`,
+//           body: model,
+//         }
+//       },
+//       invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
+//     }),
+//   }),
+// })
+
+// export const { useGetTasksQuery, useAddTaskMutation, useRemoveTaskMutation, useUpdateTaskMutation } = tasksApi
+
+/////////////////////////////////////////////////////
+
+import { instance } from "common/instance"
 import { BaseResponse } from "common/types"
 import { baseApi } from "../../../app/baseApi"
 import { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
+
+// [
+// todoid1
+// {type: 'Task', id: taskid1},
+// {type: 'Task', id: taskid2},
+// {type: 'Task', id: taskid3},
+// {type: 'Task', id: taskid4},
+// {type: 'Task', id: todolistId1},
+
+// todoid2
+// {type: 'Task', id: taskid10},
+// {type: 'Task', id: taskid11},
+// {type: 'Task', id: taskid12},
+// {type: 'Task', id: todolistId2},
+// ]
 
 export const PAGE_SIZE = 4
 
 export const tasksApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
-    // getTasks: build.query<GetTasksResponse, string>({
-    getTasks: build.query<GetTasksResponse, { todolistId: string; args: { count: number; page: number } }>({
-      // query: (todolistId) => `todo-lists/${todolistId}/tasks`,
+    getTasks: build.query<GetTasksResponse, { todolistId: string; args: { page: number } }>({
       query: ({ todolistId, args }) => {
-        const params = { ...args, count: PAGE_SIZE }
-
-        // return {
-        //   url: `todo-lists/${todolistId}/tasks`,
-        //   params: args,
-        // }
         return {
-          method: "GET",
           url: `todo-lists/${todolistId}/tasks`,
-          params,
+          params: { ...args, count: PAGE_SIZE },
         }
       },
-      providesTags: (res, err, { todolistId }) =>
-        res
-          ? [...res.items.map(({ id }) => ({ type: "Task", id }) as const), { type: "Task", id: todolistId }]
-          : ["Task"],
+      providesTags: (result, error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
-
     addTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
       query: ({ todolistId, title }) => {
         return {
@@ -180,7 +254,7 @@ export const tasksApi = baseApi.injectEndpoints({
           },
         }
       },
-      invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
+      invalidatesTags: (result, error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
     removeTask: build.mutation<BaseResponse, { todolistId: string; taskId: string }>({
       query: ({ todolistId, taskId }) => {
@@ -189,7 +263,7 @@ export const tasksApi = baseApi.injectEndpoints({
           url: `todo-lists/${todolistId}/tasks/${taskId}`,
         }
       },
-      invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
+      invalidatesTags: (result, error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
     updateTask: build.mutation<
       BaseResponse<{ item: DomainTask }>,
@@ -202,9 +276,52 @@ export const tasksApi = baseApi.injectEndpoints({
           body: model,
         }
       },
-      invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
+      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled, getState }) {
+        const cachedArgsForQuery = tasksApi.util.selectCachedArgsForQuery(getState(), "getTasks")
+
+        let patchResults: any[] = []
+
+        cachedArgsForQuery.forEach(({ args }) => {
+          patchResults.push(
+            dispatch(
+              tasksApi.util.updateQueryData("getTasks", { todolistId, args: { page: args.page } }, (state) => {
+                const task = state.items.find((t) => t.id === taskId)
+                if (task) {
+                  task.status = model.status
+                }
+              }),
+            ),
+          )
+        })
+        try {
+          await queryFulfilled
+        } catch (e) {
+          patchResults.forEach((patchResult) => {
+            patchResult.undo()
+          })
+        }
+      },
+      invalidatesTags: (result, error, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
   }),
 })
 
 export const { useGetTasksQuery, useAddTaskMutation, useRemoveTaskMutation, useUpdateTaskMutation } = tasksApi
+
+export const _tasksApi = {
+  getTasks(todolistId: string) {
+    return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
+  },
+  createTask(payload: { title: string; todolistId: string }) {
+    const { title, todolistId } = payload
+    return instance.post<BaseResponse<{ item: DomainTask }>>(`todo-lists/${todolistId}/tasks`, { title })
+  },
+  deleteTask(payload: { todolistId: string; taskId: string }) {
+    const { taskId, todolistId } = payload
+    return instance.delete<BaseResponse>(`todo-lists/${todolistId}/tasks/${taskId}`)
+  },
+  updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
+    const { taskId, todolistId, model } = payload
+    return instance.put<BaseResponse<{ item: DomainTask }>>(`todo-lists/${todolistId}/tasks/${taskId}`, model)
+  },
+}
--- src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/Todolist/Tasks/Tasks.tsx
@@ -1,19 +1,72 @@
+// import List from "@mui/material/List"
+// import { TaskStatus } from "common/enums"
+// import { useGetTasksQuery } from "../../../../api/tasksApi"
+// import { DomainTodolist } from "../../../../lib/types/types"
+// import { TasksSkeleton } from "../../../skeletons/TasksSkeleton/TasksSkeleton"
+// import { Task } from "./Task/Task"
+
+// type Props = {
+//   todolist: DomainTodolist
+// }
+
+// export const Tasks = ({ todolist }: Props) => {
+//   // const { data, isLoading } = useGetTasksQuery(todolist.id)
+//   const { data, isLoading } = useGetTasksQuery({
+//     todolistId: todolist.id,
+//     args: { count: 4, page: 1 },
+//   })
+
+//   let tasksForTodolist = data?.items
+
+//   if (todolist.filter === "active") {
+//     tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.New)
+//   }
+
+//   if (todolist.filter === "completed") {
+//     tasksForTodolist = tasksForTodolist?.filter((task) => task.status === TaskStatus.Completed)
+//   }
+
+//   if (isLoading) {
+//     return <TasksSkeleton />
+//   }
+
+//   return (
+//     <>
+//       {tasksForTodolist?.length === 0 ? (
+//         <p>Тасок нет</p>
+//       ) : (
+//         <List>
+//           {tasksForTodolist?.map((task) => {
+//             return <Task key={task.id} task={task} todolist={todolist} />
+//           })}
+//         </List>
+//       )}
+//     </>
+//   )
+// }
+
+///////////////////////////////////////////
+
 import List from "@mui/material/List"
 import { TaskStatus } from "common/enums"
 import { useGetTasksQuery } from "../../../../api/tasksApi"
 import { DomainTodolist } from "../../../../lib/types/types"
 import { TasksSkeleton } from "../../../skeletons/TasksSkeleton/TasksSkeleton"
 import { Task } from "./Task/Task"
+import { useState } from "react"
+import { TasksPagination } from "../TasksPagination/TasksPagination"
 
 type Props = {
   todolist: DomainTodolist
 }
 
 export const Tasks = ({ todolist }: Props) => {
-  // const { data, isLoading } = useGetTasksQuery(todolist.id)
+  const [page, setPage] = useState(1)
+
   const { data, isLoading } = useGetTasksQuery({
     todolistId: todolist.id,
-    args: { count: 4, page: 1 },
+    // args: { count: 4, page: 1 },
+    args: { page },
   })
 
   let tasksForTodolist = data?.items
@@ -35,11 +88,15 @@ export const Tasks = ({ todolist }: Props) => {
       {tasksForTodolist?.length === 0 ? (
         <p>Тасок нет</p>
       ) : (
-        <List>
-          {tasksForTodolist?.map((task) => {
-            return <Task key={task.id} task={task} todolist={todolist} />
-          })}
-        </List>
+        <>
+          <List>
+            {tasksForTodolist?.map((task) => {
+              return <Task key={task.id} task={task} todolist={todolist} />
+            })}
+          </List>
+
+          <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />
+        </>
       )}
     </>
   )
